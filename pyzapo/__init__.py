bc237924e23453b80a2ad8d87df0e4d68d29f061
Artur <melnikarthur280912@gmail.com>
2026-09-19 15:29:23 +0300

    Initialize pyzapo module with layers and losses

---
+++ pyzapo/__init__.py
@@ -0,0 +1,4 @@
+from .layers import Linear, Sigmoid, ReLU, Sequential, Module
+from .losses import MSELoss, BCELoss
+
+__all__ = ['Linear', 'Sigmoid', 'ReLU', 'Sequential', 'Module', 'MSELoss', 'BCELoss']
