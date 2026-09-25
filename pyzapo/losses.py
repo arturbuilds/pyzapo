b0000be7f633180fa285c1e4056e9b9d081c5fd9
Artur <melnikarthur280912@gmail.com>
2026-09-25 21:27:51 +0300

    Refactor loss functions and add CrossEntropyLoss class

--- pyzapo/losses.py
+++ pyzapo/losses.py
@@ -23,10 +23,30 @@ class BCELoss:
         return -np.mean(loss)
 
     def backward(self):
-        y_pred = self.y_pred
-        y_true = self.y_true
-
-        return (y_pred - y_true) / (y_pred * (1 - y_pred)) / y_true.shape[0]
+        return ((self.y_pred - self.y_true) / (self.y_pred * (1 - self.y_pred))) / self.y_true.size
     
     def __call__(self, y_pred, y_true):
         return self.forward(y_pred, y_true)
+
+class CrossEntropyLoss:
+    def forward(self, logits, y_true):
+        self.y_true = y_true
+
+        vals_max = np.max(logits, axis=-1, keepdims=True)
+    
+        s = logits - vals_max
+        e = np.exp(s)
+        p = e / np.sum(e, axis=-1, keepdims=True)
+
+        self.y_pred = p
+        self.y_pred = np.clip(p, 1e-15, 1 - 1e-15)
+
+        loss = -np.sum(y_true * np.log(self.y_pred)) / y_true.size
+
+        return loss
+
+    def backward(self):
+        return (self.y_pred - self.y_true) / self.y_true.size
+
+    def __call__(self, logits, y_true):
+        return self.forward(logits, y_true)
