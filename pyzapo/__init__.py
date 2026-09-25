b1947d2e88a2321c9d05964bd7c8856282eaac16
Artur <melnikarthur280912@gmail.com>
2026-09-25 21:26:41 +0300

    Add CrossEntropyLoss and Tensor to __all__ exports

--- pyzapo/__init__.py
+++ pyzapo/__init__.py
@@ -1,4 +1,5 @@
 from .layers import Linear, Sigmoid, ReLU, Sequential, Module
-from .losses import MSELoss, BCELoss
+from .losses import MSELoss, BCELoss, CrossEntropyLoss
+from .tensor import Tensor, no_grad
 
-__all__ = ['Linear', 'Sigmoid', 'ReLU', 'Sequential', 'Module', 'MSELoss', 'BCELoss']
+__all__ = ['Linear', 'Sigmoid', 'ReLU', 'Sequential', 'Module', 'MSELoss', 'BCELoss', 'CrossEntropyLoss', 'Tensor', 'no_grad']
